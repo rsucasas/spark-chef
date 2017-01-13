601367bb41f427796f3c489346ea42f4eb247dbd
karamel <jdowling@sics.se>
2017-01-13 13:37:48 +0100

    reference to cloudera notes for installing pyspark

--- recipes/pyspark.rb
+++ recipes/pyspark.rb
@@ -1,3 +1,9 @@
+
+#
+# Read notes on what needs to be done for pyspark here:
+# http://blog.cloudera.com/blog/2015/09/how-to-prepare-your-apache-hadoop-cluster-for-pyspark-jobs/
+#
+
 node.override.anaconda.version = "2.2"
 node.override.anaconda.accept_license = "yes"
 node.override.anaconda.owner = node.hadoop_spark.user
